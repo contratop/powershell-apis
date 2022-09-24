e0476a1d56c3769b0a2c847f9fed9c7d79f5157e
ContratopDev <miandroidtabletfirst@gmail.com>
2022-09-24 11:30:16 +0200

    AnonFIles MiniApi

---
+++ Library/AnonFiles.ps1
@@ -0,0 +1,55 @@
+Clear-Host
+
+$url = "https://api.anonfiles.com/upload"
+
+if(-not(get-command curl.exe)){
+    Write-Host "curl.exe not found. Please install curl.exe and try again."
+    exit
+}
+
+$FileBrowser = New-Object System.Windows.Forms.OpenFileDialog -Property @{ InitialDirectory = [Environment]::GetFolderPath('Desktop') ; Title = 'Selecciona un archivo a subir' }
+$null = $FileBrowser.ShowDialog()
+$filetarget = Get-Item -path $FileBrowser.FileName
+$filepath = $FileBrowser.FileName
+
+
+
+
+write-host "-----------" -ForegroundColor Cyan
+$filetarget
+write-host "-----------" -ForegroundColor Cyan
+
+
+write-host "---------------------"
+write-host "Estas seguro que quieres subir el archivo?"
+$continue = read-host "Escribe [upload] para proceder"
+if(-not($continue -eq "upload")){
+    write-host "Abortando..."
+    break
+}
+write-host "Uploading $filepath..." -ForegroundColor Cyan
+
+$resultraw = curl.exe -F "file=@$filepath" $url
+$result = $resultraw | ConvertFrom-Json
+write-host ""
+
+write-host "---------------------"
+if($result.status){
+    write-host "Subida completada con exito" -ForegroundColor Green
+    write-host "---------------------"
+    write-host "Link: $($result.data.file.url.full)" -ForegroundColor Cyan
+    write-host "---------------------"
+    write-host "Informacion extra:"
+    write-host "Nombre: $($result.data.file.metadata.name)" -ForegroundColor Cyan
+    write-host "Peso: $($result.data.file.metadata.size.readable)" -ForegroundColor Cyan
+    exit
+}
+else{
+    write-warning "Error al subir el archivo" 
+    write-host "---------------------"
+    write-host "Informacion extra:"
+    write-host "Error: $($result.error.type)" -ForegroundColor Yellow
+    write-host "Mensaje: $($result.error.message)" -ForegroundColor Yellow
+    write-host "Codigo de error: $($result.error.code)" -ForegroundColor Yellow
+    exit
+}
